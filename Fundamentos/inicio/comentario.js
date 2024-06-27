0812f4dd7b17d2c17794e08c4c3bb1f46c76eecb
Vinicius Gabriel <71111380+022Vinicius@users.noreply.github.com>
2024-06-27 15:34:35 -0300

    arquivos que fiz no cin

---
+++ Fundamentos/inicio/comentario.js
@@ -0,0 +1,10 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log('passo #1')
+console.log('passo #2')
+console.log('passo #3')
+
+/* Comentário de 
+
+
+
+múltiplas linhas   */
