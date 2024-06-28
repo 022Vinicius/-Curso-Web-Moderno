91a8b726e7ef7e83840ad3687bba355d17eb98a0
Vinicius Gabriel <Viniciusgabriel307308309@gmail.com>
2024-06-27 22:24:57 -0300

    acabei todo o primeiro módulo
    acabei todo a secção 3, denominada de 'Iniciando com programação'

---
+++ Fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+//PI = 3.141592
+//PI * (raio²)
+
+const PI = 3.141592;
+const raio = 10;
+
+let resultado = Math.PI *(raio * raio)
+console.log("A área da circunferência mede: " + resultado + "m²")
+
+resultado = PI *(raio * raio)
+console.log("A área da circunferência mede: " + resultado + "m²")
