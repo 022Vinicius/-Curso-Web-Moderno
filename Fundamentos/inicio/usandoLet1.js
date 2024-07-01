dd7f19dd0682cea6c2cdf2513389fbf7cdf068c8
Vinicius Gabriel <Viniciusgabriel307308309@gmail.com>
2024-07-01 20:09:03 -0300

    escopos de variáveis e um pouco de loop

---
+++ Fundamentos/inicio/usandoLet1.js
@@ -0,0 +1,8 @@
+var numero = 1
+{
+    let numero = 2
+    console.log('dentro =', numero)// let tem escopo global, de função e de bloco
+                                   // var tem escopo global e de função
+}
+
+console.log('fora =', numero)
---
+++ Fundamentos/inicio/usandoVarEmLoop.js
@@ -0,0 +1,5 @@
+for (var i = 10; i<= 30; i++ ){
+    console.log(i)
+}
+
+console.log('i =', i)
