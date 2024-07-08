b25ca6d4bce7ea2ff6975d55de9f5fb81d1930b1
Vinicius Gabriel <71111380+022Vinicius@users.noreply.github.com>
2024-07-08 10:36:00 -0300

    .

---
+++ Fundamentos/inicio/ternarios.js
@@ -0,0 +1,5 @@
+const resultado = nota => nota >=8 ? 'Aprovado': "Reprovado" // se verdadeiro, retorna a 'Aprovado', se falso, retorna 'Reprovado'
+
+console.log(resultado(8))
+console.log(resultado(5))
+console.log(resultado(8.5))
