7aabcf408b7873aaa5d5e69364d8061332f81270
Vinicius Gabriel <71111380+022Vinicius@users.noreply.github.com>
2024-07-18 10:57:56 -0300

    commits no trabalho

---
+++ controle/for1.js
@@ -0,0 +1,10 @@
+
+
+for(let contador1 = 1; contador1 <=10; contador1++){
+    console.log(`contador = ${contador1}`)
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.1]
+for(let i = 0; i < notas.length; i++){
+    console.log(`nota = ${notas[i]}`)
+}
