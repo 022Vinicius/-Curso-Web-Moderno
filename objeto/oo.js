522dc21d277cb1af33de34f8b8ed64d4bae5c097
Vinicius Gabriel <71111380+022Vinicius@users.noreply.github.com>
2024-11-26 10:02:36 -0300

    att

---
+++ objeto/oo.js
@@ -0,0 +1,17 @@
+//Não executável
+
+//Procedural
+processamento(valor1,valor2,valor3)
+
+//OO
+ oobjeto = {
+    valor1,valor2,valor3,processamento(){
+
+
+} }
+
+oobjeto.processamento() //foco a ser o objeto
+//1. principios
+//2. encapsulamento
+// 3.herança (prototype)
+//4. polimorfismo
